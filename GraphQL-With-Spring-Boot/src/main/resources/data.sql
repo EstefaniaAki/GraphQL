4d96ad01f9461953e214ccfe9152b5d656494252
EstefaniaAki <83987563+EstefaniaAki@users.noreply.github.com>
2021-09-29 18:37:27 -0500

    Add files via upload

---
+++ GraphQL-With-Spring-Boot/src/main/resources/data.sql
@@ -0,0 +1,9 @@
+insert into Address(street,city) values('Dixon','Dallas')
+insert into Address(street,city) values('Naul','Toluca')
+
+insert into Student (first_Name, last_Name, email, address_id) values('Gorge', 'Romero','romerogorge@gmail.com',1)
+insert into Student (first_Name, last_Name, email, address_id) values('Estefani', 'Arriaga','estefania.arriaga@outlook.com',2)
+
+insert into subject (subject_name, marks_obtained, student_id) values ('Java', 75,1)
+insert into subject (subject_name, marks_obtained, student_id) values ('MySql', 89,1)
+insert into subject (subject_name, marks_obtained, student_id) values ('MongoDB', 80,2)
